d50f09b5c91c7e087aea47799635e2d6922548ac
huyn <pumpkinhuyn@gmail.com>
2017-05-26 17:13:36 +0800

    new style of ToastSeekBar

--- app/src/main/java/com/huyn/demogroup/seekbar/ToastSeekBar.java
+++ app/src/main/java/com/huyn/demogroup/seekbar/ToastSeekBar.java
@@ -7,10 +7,8 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -30,12 +28,6 @@ import java.math.BigDecimal;
 import static com.huyn.demogroup.seekbar.BubbleUtils.dp2px;
 import static com.huyn.demogroup.seekbar.BubbleUtils.sp2px;
 
-/**
- * A beautiful and powerful Android custom seek bar, which has a bubble view with progress
- * appearing upon when seeking. Highly customizable, mostly demands has been considered.
- * <p/>
- * Created by woxingxiao on 2016-10-27.
- */
 public class ToastSeekBar extends View {
 
     private float mMin; // min
@@ -44,15 +36,15 @@ public class ToastSeekBar extends View {
     private int mTrackSize; // height of right-track(on the right of thumb)
     private int mSecondTrackSize; // height of left-track(on the left of thumb)
     private int mThumbRadius; // radius of thumb
+    private boolean mThumbSolid=true;//soid
     private int mTrackColor; // color of right-track
     private int mSecondTrackColor; // color of left-track
     private int mThumbColor; // color of thumb
     private long mAnimDuration; // duration of animation
     private boolean isAlwaysShowBubble; // bubble shows all time
 
-    private int mBubbleColor;// color of bubble
-    private int mBubbleTextSize; // text size of bubble-progress
-    private int mBubbleTextColor; // text color of bubble-progress
+    private int mToastTextSize; // text size of bubble-progress
+    private int mToastTextColor; // text color of bubble-progress
 
     private float mDelta; // max - min
     private float mThumbCenterX; // X coordinate of thumb's center
@@ -69,12 +61,13 @@ public class ToastSeekBar extends View {
 
     private WindowManager mWindowManager;
     private ToastView mToastView;
-    private int mBubbleRadius;
-    private float mBubbleCenterRawSolidX;
-    private float mBubbleCenterRawSolidY;
-    private float mBubbleCenterRawX;
+    private int mToastRadius;
+    private float mToastCenterRawSolidX;
+    private float mToastCenterRawSolidY;
+    private float mToastCenterRawX;
     private WindowManager.LayoutParams mLayoutParams;
     private int[] mPoint = new int[2];
+    private int minHeight;
 
     public ToastSeekBar(Context context) {
         this(context, null);
@@ -87,26 +80,29 @@ public class ToastSeekBar extends View {
     public ToastSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BubbleSeekBar, defStyleAttr, 0);
-        mMin = a.getFloat(R.styleable.BubbleSeekBar_bsb_min, 0.0f);
-        mMax = a.getFloat(R.styleable.BubbleSeekBar_bsb_max, 100.0f);
-        mProgress = a.getFloat(R.styleable.BubbleSeekBar_bsb_progress, mMin);
-        mTrackSize = a.getDimensionPixelSize(R.styleable.BubbleSeekBar_bsb_track_size, dp2px(2));
-        mSecondTrackSize = a.getDimensionPixelSize(R.styleable.BubbleSeekBar_bsb_second_track_size,
-                mTrackSize + dp2px(2));
-        mThumbRadius = a.getDimensionPixelSize(R.styleable.BubbleSeekBar_bsb_thumb_radius,
-                mSecondTrackSize + dp2px(2));
-        mTrackColor = a.getColor(R.styleable.BubbleSeekBar_bsb_track_color,
+        minHeight = dp2px(2);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ToastSeekBar, defStyleAttr, 0);
+        mMin = a.getFloat(R.styleable.ToastSeekBar_tsb_min, 0.0f);
+        mMax = a.getFloat(R.styleable.ToastSeekBar_tsb_max, 100.0f);
+        mProgress = a.getFloat(R.styleable.ToastSeekBar_tsb_progress, mMin);
+        mTrackSize = a.getDimensionPixelSize(R.styleable.ToastSeekBar_tsb_track_size, minHeight);
+        mSecondTrackSize = a.getDimensionPixelSize(R.styleable.ToastSeekBar_tsb_second_track_size, minHeight);
+        if(mSecondTrackSize < mTrackSize)
+            mSecondTrackSize = mTrackSize;
+        mThumbRadius = a.getDimensionPixelSize(R.styleable.ToastSeekBar_tsb_thumb_radius, mSecondTrackSize + minHeight);
+        if(mThumbRadius < mSecondTrackSize)
+            mThumbRadius = mSecondTrackSize + minHeight;
+        mThumbSolid = a.getBoolean(R.styleable.ToastSeekBar_tsb_thumb_solid, false);
+        mTrackColor = a.getColor(R.styleable.ToastSeekBar_tsb_track_color,
                 ContextCompat.getColor(context, R.color.colorPrimary));
-        mSecondTrackColor = a.getColor(R.styleable.BubbleSeekBar_bsb_second_track_color,
+        mSecondTrackColor = a.getColor(R.styleable.ToastSeekBar_tsb_second_track_color,
                 ContextCompat.getColor(context, R.color.colorAccent));
-        mThumbColor = a.getColor(R.styleable.BubbleSeekBar_bsb_thumb_color, mSecondTrackColor);
-        mBubbleColor = a.getColor(R.styleable.BubbleSeekBar_bsb_bubble_color, mSecondTrackColor);
-        mBubbleTextSize = a.getDimensionPixelSize(R.styleable.BubbleSeekBar_bsb_bubble_text_size, sp2px(14));
-        mBubbleTextColor = a.getColor(R.styleable.BubbleSeekBar_bsb_bubble_text_color, Color.WHITE);
-        int duration = a.getInteger(R.styleable.BubbleSeekBar_bsb_anim_duration, -1);
+        mThumbColor = a.getColor(R.styleable.ToastSeekBar_tsb_thumb_color, mSecondTrackColor);
+        mToastTextSize = a.getDimensionPixelSize(R.styleable.ToastSeekBar_tsb_toast_text_size, sp2px(14));
+        mToastTextColor = a.getColor(R.styleable.ToastSeekBar_tsb_toast_text_color, Color.WHITE);
+        int duration = a.getInteger(R.styleable.ToastSeekBar_tsb_anim_duration, -1);
         mAnimDuration = duration < 0 ? 200 : duration;
-        isAlwaysShowBubble = a.getBoolean(R.styleable.BubbleSeekBar_bsb_always_show_bubble, false);
+        isAlwaysShowBubble = a.getBoolean(R.styleable.ToastSeekBar_tsb_always_show_bubble, false);
         a.recycle();
 
         mPaint = new Paint();
@@ -116,10 +112,10 @@ public class ToastSeekBar extends View {
 
         mRectText = new Rect();
 
-        mTextSpace = dp2px(2);
+        mTextSpace = minHeight;
         mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
 
-        // init BubbleView
+        // init ToastView
         mToastView = new ToastView(context);
         mToastView.setProgressText(String.valueOf(getProgress()));
 
@@ -160,7 +156,7 @@ public class ToastSeekBar extends View {
      * Calculate radius of bubble according to the Min and the Max
      */
     private void calculateRadiusOfBubble() {
-        mPaint.setTextSize(mBubbleTextSize);
+        mPaint.setTextSize(mToastTextSize);
 
         // 计算滑到两端气泡里文字需要显示的宽度，比较取最大值为气泡的半径
         String text;
@@ -172,9 +168,9 @@ public class ToastSeekBar extends View {
         mPaint.getTextBounds(text, 0, text.length(), mRectText);
         int w2 = (mRectText.width() + mTextSpace * 2) >> 1;
 
-        mBubbleRadius = dp2px(14); // default 14dp
-        int max = Math.max(mBubbleRadius, Math.max(w1, w2));
-        mBubbleRadius = max + mTextSpace;
+        mToastRadius = dp2px(14); // default 14dp
+        int max = Math.max(mToastRadius, Math.max(w1, w2));
+        mToastRadius = max + mTextSpace;
     }
 
     @Override
@@ -200,25 +196,25 @@ public class ToastSeekBar extends View {
      * The BubbleView is added to Window by the WindowManager, so the only connection between
      * BubbleView and SeekBar is their origin raw coordinates on the screen.
      * <p>
-     * It's easy to compute the coordinates(mBubbleCenterRawSolidX, mBubbleCenterRawSolidY) of point
+     * It's easy to compute the coordinates(mToastCenterRawSolidX, mToastCenterRawSolidY) of point
      * when the Progress equals the Min. Then compute the pixel length increment when the Progress is
-     * changing, the result is mBubbleCenterRawX. At last the WindowManager calls updateViewLayout()
+     * changing, the result is mToastCenterRawX. At last the WindowManager calls updateViewLayout()
      * to update the LayoutParameter.x of the BubbleView.
      * <p>
      * 气泡BubbleView实际是通过WindowManager动态添加的一个视图，因此与SeekBar唯一的位置联系就是它们在屏幕上的
      * 绝对坐标。
-     * 先计算进度mProgress为mMin时BubbleView的中心坐标（mBubbleCenterRawSolidX，mBubbleCenterRawSolidY），
-     * 然后根据进度来增量计算横坐标mBubbleCenterRawX，再动态设置LayoutParameter.x，就实现了气泡跟随滑动移动。
+     * 先计算进度mProgress为mMin时BubbleView的中心坐标（mToastCenterRawSolidX，mToastCenterRawSolidY），
+     * 然后根据进度来增量计算横坐标mToastCenterRawX，再动态设置LayoutParameter.x，就实现了气泡跟随滑动移动。
      */
     private void locatePositionOnScreen() {
         getLocationOnScreen(mPoint);
 
-        mBubbleCenterRawSolidX = mPoint[0] + mLeft - mToastView.getMeasuredWidth() / 2f;
-        mBubbleCenterRawX = mBubbleCenterRawSolidX + mTrackLength * (mProgress - mMin) / mDelta;
-        mBubbleCenterRawSolidY = mPoint[1] - mToastView.getMeasuredHeight();
-        mBubbleCenterRawSolidY -= dp2px(24);
+        mToastCenterRawSolidX = mPoint[0] + mLeft - mToastView.getMeasuredWidth() / 2f;
+        mToastCenterRawX = mToastCenterRawSolidX + mTrackLength * (mProgress - mMin) / mDelta;
+        mToastCenterRawSolidY = mPoint[1] - mToastView.getMeasuredHeight();
+        mToastCenterRawSolidY -= dp2px(24);
         if (BubbleUtils.isMIUI()) {
-            mBubbleCenterRawSolidY += dp2px(4);
+            mToastCenterRawSolidY += dp2px(4);
         }
     }
 
@@ -247,6 +243,10 @@ public class ToastSeekBar extends View {
         // draw thumb
         mPaint.setColor(mThumbColor);
         canvas.drawCircle(mThumbCenterX, yTop, mThumbRadius, mPaint);
+        if(!mThumbSolid) {
+            mPaint.setColor(Color.WHITE);
+            canvas.drawCircle(mThumbCenterX, yTop, mThumbRadius - minHeight, mPaint);
+        }
     }
 
     @Override
@@ -309,7 +309,7 @@ public class ToastSeekBar extends View {
                         mThumbCenterX = mRight;
                     }
                     mProgress = (mThumbCenterX - mLeft) * mDelta / mTrackLength + mMin;
-                    mBubbleCenterRawX = mBubbleCenterRawSolidX + mTrackLength * (mProgress - mMin) / mDelta;
+                    mToastCenterRawX = mToastCenterRawSolidX + mTrackLength * (mProgress - mMin) / mDelta;
 
                     showBubble();
                     invalidate();
@@ -329,8 +329,8 @@ public class ToastSeekBar extends View {
                     }
                     mProgress = (mThumbCenterX - mLeft) * mDelta / mTrackLength + mMin;
 
-                    mBubbleCenterRawX = mBubbleCenterRawSolidX + mTrackLength * (mProgress - mMin) / mDelta;
-                    mLayoutParams.x = (int) (mBubbleCenterRawX + 0.5f);
+                    mToastCenterRawX = mToastCenterRawSolidX + mTrackLength * (mProgress - mMin) / mDelta;
+                    mLayoutParams.x = (int) (mToastCenterRawX + 0.5f);
                     mWindowManager.updateViewLayout(mToastView, mLayoutParams);
                     mToastView.setProgressText(String.valueOf(getProgress()));
 
@@ -444,8 +444,8 @@ public class ToastSeekBar extends View {
                 mLayoutParams.type = WindowManager.LayoutParams.TYPE_TOAST;
             }
         }
-        mLayoutParams.x = (int) (mBubbleCenterRawX + 0.5f);
-        mLayoutParams.y = (int) (mBubbleCenterRawSolidY + 0.5f);
+        mLayoutParams.x = (int) (mToastCenterRawX + 0.5f);
+        mLayoutParams.y = (int) (mToastCenterRawSolidY + 0.5f);
 
         mToastView.setAlpha(0);
         mToastView.setVisibility(VISIBLE);
@@ -470,7 +470,7 @@ public class ToastSeekBar extends View {
     }
 
     /**
-     * When BubbleSeekBar's parent view is scrollable, must listener to it's scrolling and call this
+     * When ToastSeekBar's parent view is scrollable, must listener to it's scrolling and call this
      * method to correct the offsets.
      */
     public void correctOffsetWhenContainerOnScrolling() {
@@ -500,7 +500,7 @@ public class ToastSeekBar extends View {
     public void setProgress(float progress) {
         mProgress = progress;
 
-        mBubbleCenterRawX = mBubbleCenterRawSolidX + mTrackLength * (mProgress - mMin) / mDelta;
+        mToastCenterRawX = mToastCenterRawSolidX + mTrackLength * (mProgress - mMin) / mDelta;
 
         if (mProgressListener != null) {
             mProgressListener.onProgressChanged(getProgress(), getProgressFloat());
@@ -603,13 +603,11 @@ public class ToastSeekBar extends View {
     }
 
     /*******************************************************************************************
-     * ************************************  custom toast view  ************************************
+     **************************************  custom toast view  ********************************
      *******************************************************************************************/
     private class ToastView extends View {
 
-        private Paint mBubblePaint;
-        private Path mBubblePath;
-        private RectF mBubbleRectF;
+        private Paint mToastPaint;
         private Rect mRect;
         private String mProgressText = "";
 
@@ -624,12 +622,10 @@ public class ToastSeekBar extends View {
         ToastView(Context context, AttributeSet attrs, int defStyleAttr) {
             super(context, attrs, defStyleAttr);
 
-            mBubblePaint = new Paint();
-            mBubblePaint.setAntiAlias(true);
-            mBubblePaint.setTextAlign(Paint.Align.CENTER);
+            mToastPaint = new Paint();
+            mToastPaint.setAntiAlias(true);
+            mToastPaint.setTextAlign(Paint.Align.CENTER);
 
-            mBubblePath = new Path();
-            mBubbleRectF = new RectF();
             mRect = new Rect();
         }
 
@@ -637,44 +633,20 @@ public class ToastSeekBar extends View {
         protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-            setMeasuredDimension(3 * mBubbleRadius, 3 * mBubbleRadius);
-
-            mBubbleRectF.set(getMeasuredWidth() / 2f - mBubbleRadius, 0,
-                    getMeasuredWidth() / 2f + mBubbleRadius, 2 * mBubbleRadius);
+            setMeasuredDimension(2 * mToastRadius, 2 * mToastRadius);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            mBubblePath.reset();
-            float x0 = getMeasuredWidth() / 2f;
-            float y0 = getMeasuredHeight() - mBubbleRadius / 3f;
-            mBubblePath.moveTo(x0, y0);
-            float x1 = (float) (getMeasuredWidth() / 2f - Math.sqrt(3) / 2f * mBubbleRadius);
-            float y1 = 3 / 2f * mBubbleRadius;
-            mBubblePath.quadTo(
-                    x1 - dp2px(2), y1 - dp2px(2),
-                    x1, y1
-            );
-            mBubblePath.arcTo(mBubbleRectF, 150, 240);
-
-            float x2 = (float) (getMeasuredWidth() / 2f + Math.sqrt(3) / 2f * mBubbleRadius);
-            mBubblePath.quadTo(
-                    x2 + dp2px(2), y1 - dp2px(2),
-                    x0, y0
-            );
-            mBubblePath.close();
-
-            mBubblePaint.setColor(mBubbleColor);
-            canvas.drawPath(mBubblePath, mBubblePaint);
-
-            mBubblePaint.setTextSize(mBubbleTextSize);
-            mBubblePaint.setColor(mBubbleTextColor);
-            mBubblePaint.getTextBounds(mProgressText, 0, mProgressText.length(), mRect);
-            Paint.FontMetrics fm = mBubblePaint.getFontMetrics();
-            float baseline = mBubbleRadius + (fm.descent - fm.ascent) / 2f - fm.descent;
-            canvas.drawText(mProgressText, getMeasuredWidth() / 2f, baseline, mBubblePaint);
+            mToastPaint.setTextSize(mToastTextSize);
+            mToastPaint.setColor(mToastTextColor);
+            String text = mProgressText + "%";
+            mToastPaint.getTextBounds(text, 0, text.length(), mRect);
+            Paint.FontMetrics fm = mToastPaint.getFontMetrics();
+            float baseline = mToastRadius + (fm.descent - fm.ascent) / 2f - fm.descent;
+            canvas.drawText(text, getMeasuredWidth() / 2f, baseline, mToastPaint);
         }
 
         void setProgressText(String progressText) {
