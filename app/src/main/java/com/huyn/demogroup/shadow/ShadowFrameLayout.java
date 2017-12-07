6c28adb0e8ac200d3b99c33f1a3703cdd9477c59
huyn <pumpkinhuyn@gmail.com>
2017-12-07 17:07:44 +0800

    optimized shadow layout

--- app/src/main/java/com/huyn/demogroup/shadow/ShadowFrameLayout.java
+++ app/src/main/java/com/huyn/demogroup/shadow/ShadowFrameLayout.java
@@ -7,40 +7,38 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 
 /**
  * Created by huyaonan on 2017/12/6.
  */
 
-public class ShadowLinearLayout extends LinearLayout {
+public class ShadowFrameLayout extends FrameLayout {
 
     private Paint mPaint;
 
-    public ShadowLinearLayout(Context context) {
+    public ShadowFrameLayout(Context context) {
         this(context, null);
     }
 
-    public ShadowLinearLayout(Context context, @Nullable AttributeSet attrs) {
+    public ShadowFrameLayout(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public ShadowLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public ShadowFrameLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setLayerType(LAYER_TYPE_SOFTWARE, null);
 
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-        mPaint.setColor(Color.BLACK);
+        mPaint.setColor(Color.WHITE);
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
-        int w = getWidth();
-        int h = getHeight();
         int padding = getPaddingLeft();
-        mPaint.setMaskFilter(new BlurMaskFilter(padding/2, BlurMaskFilter.Blur.SOLID));
-        canvas.drawRect(padding, padding, w-padding, h-padding, mPaint);
+        mPaint.setShadowLayer(padding/2, 0, padding/4, Color.BLACK);
+        canvas.drawRect(padding, padding, getWidth()-padding, getHeight()-padding, mPaint);
         super.dispatchDraw(canvas);
     }
 }
